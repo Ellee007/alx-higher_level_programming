1e64230095c3fbb28a98bf3cf93b4fa220170823
Ellee007 <alhassanaliyu83@gmail.com>
2024-01-16 07:48:13 -0800

    first set

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create database
+-- Query to create database if not already created
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes database
+-- Query to delete database if present
+DROP DATABASE IF EXISTS hbtn_0c_0;
